9251969f9739c9b02fbda109ea9eaf8e319db0c8
van Thinh Le <vanthinhle@web.de>
2020-06-21 13:24:28 +0200

    add resource handler swagger ui, to make swagger work again

--- src/main/java/org/nebenkosten/config/WebConfig.java
+++ src/main/java/org/nebenkosten/config/WebConfig.java
@@ -3,6 +3,7 @@ package org.nebenkosten.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins("http://localhost:4200", "https://nebenkosten.herokuapp.com");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 	}
 }
