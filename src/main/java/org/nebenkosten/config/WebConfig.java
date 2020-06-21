4521e1ac2d025fdb38c953f110e2b435e90bbbd0
van Thinh Le <vanthinhle@web.de>
2020-06-21 13:30:30 +0200

    additional mapping for swagger

--- src/main/java/org/nebenkosten/config/WebConfig.java
+++ src/main/java/org/nebenkosten/config/WebConfig.java
@@ -18,5 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 }
